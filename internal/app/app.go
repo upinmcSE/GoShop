9168d40e1e833e19a26333fd2501d591a5395b8e
Duong Trung Thanh <thanhdt3019@gmail.com>
2025-08-11 16:46:32 +0700

    handler graceful shutdown

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,7 +1,13 @@
 package app
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -43,7 +49,38 @@ func NewApplication(cfg *config.Config) *Application {
 }
 
 func (a *Application) Run() error {
-	return a.router.Run(a.config.ServerAddress)
+	// return a.router.Run(a.config.ServerAddress)
+	srv := &http.Server{
+		Addr: a.config.ServerAddress,
+		Handler: a.router,
+	}
+
+	quit := make(chan os.Signal, 1)
+	// syscall.SIGINT -> Ctrl + C
+	// syscall.SIGTERM -> Kill service
+	// syscall.SIGHUP -> Reaload service
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+
+	go func(){
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed{
+			log.Fatalf("Faild to start server: %v", err)
+		}
+	}()
+
+	// Lệnh này sẽ block cho đến khi nhận được tín hiệu
+	// Đây là channel receive operation trong Go.
+	// Nó sẽ chặn (block) chương trình lại cho đến khi channel quit có ít nhất 1 giá trị được gửi vào.
+	// Khi có giá trị, nó sẽ lấy giá trị đó ra và tiếp tục chạy code tiếp theo.
+	<- quit
+
+	ctx, cancel :=context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("server forced to shutdown: %v", err)
+	}
+
+	return nil
 }
 
 func getModulRoutes(modules []Module) []routes.Route {
